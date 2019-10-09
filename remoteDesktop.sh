89997d1997e9c57ade07393a4fe797b9f5a2bca9
Ben Larabie <benlarabie@gmail.com>
2019-10-09 14:01:16 -0400

    busybox and rdesktop

--- remoteDesktop.sh
+++ remoteDesktop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-REMOTEPORT=$(netstat -an | grep LISTEN | grep 127.0.0.1:[3-4][0-5][0-9][0-9][0-9] | grep -o [3-4][0-5][0-9][0-9][0-9])
+if [ -z "$1" ];
+then
+REMOTEPORT=$(netstat -an | grep LISTEN | grep 127.0.0.1:[3-4][0-7][0-9][0-9][0-9] | grep -o [3-4][0-5][0-9][0-9][0-9])
 rdesktop -g 1600x900 localhost:$REMOTEPORT
+else
+rdesktop -g 1600x900 localhost:$1
+fi
