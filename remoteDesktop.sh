60ab7e42c4172bd9c29e7d66d9e8c26721c278c6
Ben Larabie <benlarabie@gmail.com>
2019-11-13 12:27:27 -0500

    Changing port filter on remote desktop

--- remoteDesktop.sh
+++ remoteDesktop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ];
 then
-REMOTEPORT=$(netstat -an | grep LISTEN | grep 127.0.0.1:[3-4][0-7][0-9][0-9][0-9] | grep -o [3-4][0-5][0-9][0-9][0-9])
+REMOTEPORT=$(netstat -an | grep LISTEN | grep 127.0.0.1:[3-4][0-9][0-9][0-9][0-9] | grep -o [3-4][0-9][0-9][0-9][0-9])
 rdesktop -g 1600x900 localhost:$REMOTEPORT
 else
 rdesktop -g 1600x900 localhost:$1
